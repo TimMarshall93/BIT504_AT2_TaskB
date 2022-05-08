b7a5ebe0df60dc4fdb888d50d53c3536eaf74dbe
TimMarshall93 <tmtimtam@gmail.com>
2022-05-08 22:14:02 +1200

    Update invokeLater in Pong and new class PongPanel added.

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -8,18 +8,26 @@ public class Pong extends JFrame
 	private static final String windowTitle = "Pong";
 	private static final Integer windowWidth = 800;
 	private static final Integer windowHeight = 600;
-	
+
 	public Pong() 
 	{
     	setTitle(windowTitle);
 	    setSize(windowWidth, windowHeight);
     	setResizable(false);
+    	add(new PongPanel());
     	setVisible(true);
     	setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
-	public static void main(String[] args) {
-		new Pong();
+	public static void main(String[] args) 
+	{
+		javax.swing.SwingUtilities.invokeLater(new Runnable() 
+		{
+	        public void run() 
+	        {
+		        new Pong();
+	        }
+		});
 
 	}
 
